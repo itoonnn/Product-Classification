0963a1aa14cb2189cdd689405c2537b3ec353e6d
itoonnn <itoonnn@gmail.com>
2017-04-10 04:51:46 +0700

    Naive bayes fn finish

--- process.py
+++ process.py
@@ -12,11 +12,14 @@ from sklearn import cross_validation
 
 def GRID_FN(power):
   return 10**(power)
+
 def roc_auc_fixed(y,y_pred):
   pos_label = max(y)
   fpr, tpr, thresholds = roc_curve(y,y_pred,pos_label=pos_label)
   auc_score = auc(fpr,tpr)
   return auc_score
+roc_auc_my = make_scorer(roc_auc_fixed)
+
 def getResult(predictions,labelData,probas_):
   pos_label = max(labelData)
   acc = accuracy_score(labelData,predictions)
@@ -24,74 +27,92 @@ def getResult(predictions,labelData,probas_):
   # report = classification_report(labelData,predictions)
   auc_score = roc_auc_fixed(labelData,predictions)
   return acc,precision,recall,fbeta,auc_score
+
+def predict_process(data,labelData,clf):
+  predictions = clf.predict(data)
+  probas_ = clf.predict_proba(data)
+  acc,precision,recall,fbeta,auc_score = getResult(predictions,labelData,probas_)
+  return acc,precision,recall,fbeta,auc_score
+
 def naivebeys_process(SEED,GROUP,START,END,train,test,label_train,label_test):
   print("\nMultinomialNB")
   END += 1
-  fname = "crossval_nb_result_"+str(GROUP)+".csv"
+  fname = "crossval_nb_result.csv"
   #Tune Alpha
   alpha = 0.0
-  scores_mean_nv = []
-  scores_std_nv = []
-  alphas = ()
-
-  dtype = [('ngram',int),('alpha',float),('score',float),('std',float)]
-  scores = pd.DataFrame(dtype = dtype)
-  if(os.path.isfile(fname) ):
-    scores.to_csv(fname)
+  parameter_all = np.array([GRID_FN(i) for i in range(-5,6)])
+  print("All param")
+  print(parameter_all)
+  columns = ['seed','alpha','score','std']
+  df = pd.DataFrame(columns = columns)
+  if(not os.path.isfile(fname)):
+    df.to_csv(fname)
+    print("create ",fname)
+  else:
+    data = df.from_csv(fname)
+    print("Exist param")
+    parameter_exist = np.array(data['alpha'])
+    print(parameter_exist)
   #tune parameter
   for power in range(START,END,1):
     alpha = GRID_FN(power)
-    alphas+=(alpha,)
-    #cross validation
-    clf = MultinomialNB(alpha=alpha)
-    scores = cross_validation.cross_val_score(clf,train,label_train,cv=10,scoring=roc_auc_my)
-    # print scores
-    print(ngram,alpha,scores.mean(), scores.std())
-    scores_mean_nv.append(scores.mean())
-    scores_std_nv.append(scores.std())
-    ### write file
-    
+    if(alpha not in parameter_exist):
+      #cross validation
+      clf = MultinomialNB(alpha=alpha)
+      scores = cross_validation.cross_val_score(clf,train,label_train,cv=10,scoring=roc_auc_my)
+      score = round(scores.mean(),3)
+      std = round(scores.std(),3)
+      print(GROUP,alpha,score,std)
+      df = pd.DataFrame([{'seed':GROUP,'alpha':alpha,'score':score,'std':std}],columns = columns)
+      ### write file
+      with open(fname, 'a') as f:
+        df.to_csv(f, header=False)
+      
+  data = df.from_csv(fname)
+  parameter_exist = np.array(data['alpha'])
+  if np.array_equal(parameter_all, parameter_exist):
+    print("Complete all parameter =*=*=*=")
+    rec_max = data.sort(['score','std'],ascending=[0,1]).head(1)
+    alpha_max = float(rec_max['alpha'])
 
-    ###
+    ### prediction start ###
+    ## classified training
+    clf = MultinomialNB(alpha=alpha_max).fit(train, label_train)
+    print(clf)
 
-  # scores_mean_nv = np.array(scores_mean_nv)
-  # scores_std_nv = np.array(scores_std_nv)
-  
-  # scores_zip = zip(ngrams,alphas,scores_mean_nv,scores_std_nv)
-  # scores_list = pd.DataFrame(np.array(scores_zip,dtype=dtype))
+    # Check accuracy but this is based on the same data we used for training
+    # Use classifier to train and test
+    print('Result with NaiveBeys =*=*=*=*=')
+    train_acc,train_precision,train_recall,train_f1,train_auc = predict_process(train,label_train,clf)
+    test_acc,test_precision,test_recall,test_f1,test_auc = predict_process(test,label_test,clf)
 
-  # scores_max_nv = scores_list.sort(['score','std'],ascending=[0,1]).head(1)
-  # max_alpha_nv = scores_max_nv.get_value(scores_max_nv.index.values[0],'alpha')
-
-  # print "NV score : "
-  # print scores_max_nv
-
-  # ### prediction start ###
-  # ## classified training
-  # clf = MultinomialNB(alpha=max_alpha_nv).fit(train, label_train)
-  # print(clf)
-
-  # # Check accuracy but this is based on the same data we used for training
-  # # Use classifier to train and test
-  # print 'Result of Generation ',gen,' with NaiveBeys'
-  # train_acc,train_precision,train_recall,train_f1,train_auc = predict_process("train",train,label_train,clf)
-  # test_acc,test_precision,test_recall,test_f1,test_auc = predict_process("test",test,label_test,clf)
-
-  # print "ngram : ",ngram
-  # print "alpha : ",max_alpha_nv
-  # dtype = [('ngram',int),('alpha',float),('train_acc',float),('train_precision',float),('train_recall',float),('train_f1',float),('train_auc',float),('test_acc',float),('test_precision',float),('test_recall',float),('test_f1',float),('test_auc',float)]
-  # scores_result = pd.DataFrame(np.array([(
-  #   ngram,
-  #   max_alpha_nv,
-  #   train_acc,
-  #   train_precision,
-  #   train_recall,
-  #   train_f1,
-  #   train_auc,
-  #   test_acc,
-  #   test_precision,
-  #   test_recall,
-  #   test_f1,
-  #   test_auc
-  # )],dtype=dtype))
-  # return scores_list,scores_result,train_report,test_report
+    columns = [
+      'seed',
+      'alpha',
+      'train_acc',
+      'train_precision',
+      'train_recall',
+      'train_f1',
+      'train_auc',
+      'test_acc',
+      'test_precision',
+      'test_recall',
+      'test_f1',
+      'test_auc',
+    ]
+    result = pd.DataFrame(pd.Series({
+      'seed':GROUP,
+      'alpha':alpha_max,
+      'train_acc':train_acc,
+      'train_precision':train_precision,
+      'train_recall':train_recall,
+      'train_f1':train_f1,
+      'train_auc':train_auc,
+      'test_acc':test_acc,
+      'test_precision':test_precision,
+      'test_recall':test_recall,
+      'test_f1':test_f1,
+      'test_auc':test_auc,
+    }))
+    return result
+  return 0
