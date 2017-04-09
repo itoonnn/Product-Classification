6eb49b747dd350fc20491cb50bf67413ba09a75c
ITKMITL_HP04\HP_PC01 <itoonnn@gmail.com>
2017-04-09 20:09:39 +0700

    Rebuild Image Classification

--- classification_image.py
+++ classification_image.py
@@ -1,110 +1,68 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import StratifiedShuffleSplit,StratifiedKFold
-from sklearn.preprocessing import LabelEncoder,FunctionTransformer,Normalizer,label_binarize,MultiLabelBinarizer
-from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matrix, precision_score ,recall_score, roc_curve, auc, roc_auc_score,make_scorer,precision_recall_fscore_support
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import SVC
 from extractImage import *
-
-def roc_auc_fixed(y,y_pred):
-  pos_label = max(y)
-  fpr, tpr, thresholds = roc_curve(y,y_pred,pos_label=pos_label)
-  auc_score = auc(fpr,tpr)
-  return auc_score
-def getResult(predictions,labelData,probas_):
-  pos_label = max(labelData)
-  acc = accuracy_score(labelData,predictions)
-  (precision,recall,fbeta,support) = precision_recall_fscore_support(labelData,predictions,pos_label=pos_label,average="weighted")
-  # report = classification_report(labelData,predictions)
-  auc_score = roc_auc_fixed(labelData,predictions)
-  return acc,precision,recall,fbeta,auc_score
+from process import *
 
 TEST_SIZE = 0.2
-# Specify input csv file
-print("file")
-print("coldstorage_path.csv == 1")
-print("giant_path.csv == 2")
-print("redmart_path.csv == 3")
-input_file = input()
-input_file = int(input_file)
-if input_file == 1:
-  input_file = "coldstorage_path.csv"
-elif input_file == 2:
-  input_file = "giant_path.csv"
-elif input_file == 3:
-  input_file = "redmart_path.csv"
-img_root = input_file.replace("_path.csv","")+"_img"
-print("Opertion contextural,sift,surf,orb")
-opt = input()
-print("SEED")
 SEED = 2000
-GROUP = int(input())
-print(input_file)
-df = pd.read_csv(input_file, header = 0)
+STORE = ["coldstorage","fairprice","giant","redmart"]
+FUNCTION = ["contextual","sift","surf","orb"]
+num_k = ["contextual","sqrt(n)","sqrt(half(n))"]
+PATH = "image_feature/"
 
+# Specify input csv file
+print("STORE")
+print("coldstorage == 0")
+print("fairprice == 1")
+print("giant == 2")
+print("redmart == 3")
+STORE = STORE[int(input())]
 
-### Preprocessing  start ###
-###
-# Subset dataframe to just columns category_path and name
-df = df.loc[:,['category_path','name','img_file']]
-# Make a duplicate of input df
-df_original=df
-df_dedup=df.drop_duplicates(subset='name')
-# print(len(np.unique(df_dedup['name'])))
-df=df_dedup
-#drop paths that have 1 member
-df_count = df.groupby(['category_path']).count()
-df_count = df_count[df_count == 1]
-df_count = df_count.dropna()
-df = df.loc[~df['category_path'].isin(list(df_count.index))]
-df = df.reset_index(drop=True)
-print("Uniqued df by name : "+str(len(df['name'])))
-####### label encoder
-number = LabelEncoder()
-y = df['category_path']
-y = y.astype(str) 
-y = number.fit_transform(y)
-print(np.shape(np.unique(y)))
-##### image processing
+print("FUNCTION")
+print("contextual == 0")
+print("sift == 1")
+print("surf == 2")
+print("orb == 3")
+FUNCTION = FUNCTION[int(input())]
 
-train,test,label_train,label_test = extractImageFeature(df['img_file'],y,img_root,opt=opt,random_state=SEED)
-print(x)
-# #### normalize img
-# norm = Normalizer()
-# x = norm.fit_transform(x)
+if(FUNCTION in ["sift","surf","orb"]):
+  print("Root n : 1 or Root half n : 2")
+  num_k = num_k[int(input())]
+else:
+  num_k = num_k[0]
 
-# y = y[:20]
-####
-### Preprocessing end ###
+PATH += num_k
 
-# output = pd.DataFrame(pd.Series(x))
-# np.savetxt("image_feature_"+opt+"_"+str(GROUP)+".csv",x,delimiter=',')
-# np.savetxt("image_label.csv",y)
+print("CLASSIFIER")
+print("Naive Bayes == 0")
+print("Logitic Regression == 1")
+print("Neural Network == 2")
+print("SVM-Linear == 3")
+print("SVM-RBF == 4")
+print("SVM-Poly == 5")
+CLASSIFIER = int(input())
 
+for i in range(10):
+  file_train = PATH+"/"+"_".join(["feature",STORE,FUNCTION,"train",str(i)])+".csv"
+  file_test = PATH+"/"+"_".join(["feature",STORE,FUNCTION,"test",str(i)])+".csv"
+  file_train_label = PATH+"/"+"_".join(["label",STORE,FUNCTION,"train",str(i)])+".csv"
+  file_test_label = PATH+"/"+"_".join(["label",STORE,FUNCTION,"test",str(i)])+".csv"
+  train = np.loadtxt(file_train,delimiter=',')
+  test = np.loadtxt(file_test,delimiter=',')
+  label_train = np.loadtxt(file_train_label,delimiter=',')
+  label_test = np.loadtxt(file_test_label,delimiter=',')
+  if(CLASSIFIER == 0):
 
-# #### split train test
-# SSK = StratifiedKFold(n_splits=10,random_state=SEED)
-# INDEX = []
-# for train_index, test_index in SSK.split(x,y):
-#   INDEX.append({'train':train_index,'test':test_index})
-# train = x[INDEX[GROUP]['train']]
-# test = x[INDEX[GROUP]['test']]
-# label_train = y[INDEX[GROUP]['train']]
-# label_test = y[INDEX[GROUP]['test']]
-
-# unique, counts = np.unique(label_train, return_counts=True)
-# print(np.asarray((unique,counts)).T)
 
 ##### classification
-print(train)
-clf = SVC(C=1,probability=True,random_state=SEED).fit(train, label_train)
-pred = clf.predict(train)
-probas_ = clf.predict_proba(train)
-acc,precision,recall,fbeta,auc_score = getResult(pred,label_train,probas_)
-print("TRAIN RESULT")
-print("accuracy :",acc)
-print("precision :",precision)
-print("recall :",recall)
-print("f-score :",fbeta)
-print("AUROC :",auc_score)
+# clf = SVC(C=1,probability=True,random_state=SEED).fit(train, label_train)
+
+# pred = clf.predict(train)
+# probas_ = clf.predict_proba(train)
+# acc,precision,recall,fbeta,auc_score = getResult(pred,label_train,probas_)
+# print("TRAIN RESULT")
+# print("accuracy :",acc)
+# print("precision :",precision)
+# print("recall :",recall)
+# print("f-score :",fbeta)
+# print("AUROC :",auc_score)
---
+++ process.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+import os
+from sklearn.model_selection import StratifiedShuffleSplit,StratifiedKFold
+from sklearn.preprocessing import LabelEncoder,FunctionTransformer,Normalizer,label_binarize,MultiLabelBinarizer
+from sklearn.metrics import f1_score, accuracy_score, confusion_matrix, precision_score ,recall_score, roc_curve, auc, roc_auc_score,make_scorer,precision_recall_fscore_support
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn import cross_validation
+
+def GRID_FN(power):
+  return 10**(power)
+def roc_auc_fixed(y,y_pred):
+  pos_label = max(y)
+  fpr, tpr, thresholds = roc_curve(y,y_pred,pos_label=pos_label)
+  auc_score = auc(fpr,tpr)
+  return auc_score
+def getResult(predictions,labelData,probas_):
+  pos_label = max(labelData)
+  acc = accuracy_score(labelData,predictions)
+  (precision,recall,fbeta,support) = precision_recall_fscore_support(labelData,predictions,pos_label=pos_label,average="weighted")
+  # report = classification_report(labelData,predictions)
+  auc_score = roc_auc_fixed(labelData,predictions)
+  return acc,precision,recall,fbeta,auc_score
+def naivebeys_process(SEED,GROUP,START,END,train,test,label_train,label_test):
+  print "\nMultinomialNB"
+  fname = "crossval_nb_result_"+str(GROUP)+".csv"
+  #Tune Alpha
+  alpha = 0.0
+  scores_mean_nv = []
+  scores_std_nv = []
+  alphas = ()
+
+  dtype = [('ngram',int),('alpha',float),('score',float),('std',float)]
+  scores = pd.DataFrame(dtype = dtype)
+  #tune parameter
+  for power in range(START,END,1):
+    alpha = GRID_FN(power)
+    alphas+=(alpha,)
+    #cross validation
+    clf = MultinomialNB(alpha=alpha)
+    scores = cross_validation.cross_val_score(clf,train,label_train,cv=10,scoring=roc_auc_my)
+    # print scores
+    print ngram,alpha,scores.mean(), scores.std()
+    scores_mean_nv.append(scores.mean())
+    scores_std_nv.append(scores.std())
+    ### write file
+    if(os.path.isfile(fname) ):
+    else:
+
+    ###
+
+  # scores_mean_nv = np.array(scores_mean_nv)
+  # scores_std_nv = np.array(scores_std_nv)
+  
+  # scores_zip = zip(ngrams,alphas,scores_mean_nv,scores_std_nv)
+  # scores_list = pd.DataFrame(np.array(scores_zip,dtype=dtype))
+
+  # scores_max_nv = scores_list.sort(['score','std'],ascending=[0,1]).head(1)
+  # max_alpha_nv = scores_max_nv.get_value(scores_max_nv.index.values[0],'alpha')
+
+  # print "NV score : "
+  # print scores_max_nv
+
+  # ### prediction start ###
+  # ## classified training
+  # clf = MultinomialNB(alpha=max_alpha_nv).fit(train, label_train)
+  # print(clf)
+
+  # # Check accuracy but this is based on the same data we used for training
+  # # Use classifier to train and test
+  # print 'Result of Generation ',gen,' with NaiveBeys'
+  # train_acc,train_precision,train_recall,train_f1,train_auc = predict_process("train",train,label_train,clf)
+  # test_acc,test_precision,test_recall,test_f1,test_auc = predict_process("test",test,label_test,clf)
+
+  # print "ngram : ",ngram
+  # print "alpha : ",max_alpha_nv
+  # dtype = [('ngram',int),('alpha',float),('train_acc',float),('train_precision',float),('train_recall',float),('train_f1',float),('train_auc',float),('test_acc',float),('test_precision',float),('test_recall',float),('test_f1',float),('test_auc',float)]
+  # scores_result = pd.DataFrame(np.array([(
+  #   ngram,
+  #   max_alpha_nv,
+  #   train_acc,
+  #   train_precision,
+  #   train_recall,
+  #   train_f1,
+  #   train_auc,
+  #   test_acc,
+  #   test_precision,
+  #   test_recall,
+  #   test_f1,
+  #   test_auc
+  # )],dtype=dtype))
+  # return scores_list,scores_result,train_report,test_report
